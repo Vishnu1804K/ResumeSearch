b91fe3bfa1d2cd066e691953283a5a0d64b2377c
Kancherla Sri Vishnu <101489168+Vishnu1804K@users.noreply.github.com>
2024-07-20 21:19:31 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/user/users');
+        const response = await axios.get('https://resumesearchmain.onrender.com/api/user/users');
         console.log(response.data);
       } catch (error) {
         console.log(error);
